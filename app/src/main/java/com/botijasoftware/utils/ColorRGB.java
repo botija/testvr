7b38b321849879130b2cfc90445781ac32bbdae4
botija <benedicto.miguel@gmail.com>
2016-05-31 21:39:50 +0200

    ColorHSV y modificaciones a ColorRGB

--- app/src/main/java/com/botijasoftware/utils/ColorRGB.java
+++ app/src/main/java/com/botijasoftware/utils/ColorRGB.java
@@ -42,6 +42,37 @@ public class ColorRGB {
 			B *= m;
 		}
 	}
+
+	public ColorHSV toHSV() {
+		ColorHSV hsv = new ColorHSV(0);
+		float max = Math.max(R, Math.max(G,B));
+		float min = Math.min(R, Math.min(G,B));
+
+		float delta = max - min;
+
+		float h = 0.0f;
+		if (delta != 0) {
+            if (max == R) {
+                h = 60 * (((G - B) / delta) % 6);
+            }
+            else if (max == G) {
+                h = 60 * (((B - R) / delta) + 2);
+            }
+            else if (max == B) {
+                h = 60 * (((R - G) / delta) + 4);
+            }
+
+		}
+
+		float s = 0.0f;
+		if (max != 0.0f) {
+			s = delta / max;
+		}
+
+		hsv.S = s;
+		hsv.V = max;
+		return hsv;
+	}
 	
 	public float R;
 	public float G;
@@ -49,5 +80,19 @@ public class ColorRGB {
 	
 	public static final ColorRGB BLACK = new ColorRGB(0.0f, 0.0f, 0.0f);
 	public static final ColorRGB WHITE = new ColorRGB(1.0f, 1.0f, 1.0f);
+    public static final ColorRGB RED = new ColorRGB(1.0f, 0.0f, 0.0f);
+    public static final ColorRGB LIME = new ColorRGB(0.0f, 1.0f, 0.0f);
+    public static final ColorRGB BLUE = new ColorRGB(0.0f, 0.0f, 1.0f);
+    public static final ColorRGB YELLOW = new ColorRGB(1.0f, 1.0f, 0.0f);
+    public static final ColorRGB CYAN = new ColorRGB(0.0f, 1.0f, 1.0f);
+    public static final ColorRGB MAGENTA = new ColorRGB(1.0f, 0.0f, 1.0f);
+    public static final ColorRGB SILVER = new ColorRGB(0.75f, 0.75f, 0.75f);
+    public static final ColorRGB GRAY = new ColorRGB(0.5f, 0.5f, 0.5f);
+    public static final ColorRGB MAROON = new ColorRGB(0.5f, 0.0f, 0.0f);
+    public static final ColorRGB OLIVE = new ColorRGB(0.5f, 0.5f, 0.0f);
+    public static final ColorRGB GREEN = new ColorRGB(0.0f, 0.5f, 0.0f);
+    public static final ColorRGB PURPLE = new ColorRGB(0.5f, 0.0f, 0.5f);
+    public static final ColorRGB TEAL = new ColorRGB(0.0f, 0.5f, 0.5f);
+    public static final ColorRGB NAVY = new ColorRGB(0.0f, 0.0f, 0.5f);
 	
 }
--- app/src/main/java/com/botijasoftware/utils/ColorRGBb.java
+++ app/src/main/java/com/botijasoftware/utils/ColorRGBb.java
@@ -49,5 +49,19 @@ public class ColorRGBb {
 	
 	public static final ColorRGBb BLACK = new ColorRGBb((byte)0, (byte)0, (byte)0);
 	public static final ColorRGBb WHITE = new ColorRGBb((byte)255, (byte)255, (byte)255);
+	public static final ColorRGBb RED = new ColorRGBb((byte)255,(byte)0,(byte)0);
+	public static final ColorRGBb LIME = new ColorRGBb((byte)0,(byte)255,(byte)0);
+	public static final ColorRGBb BLUE = new ColorRGBb((byte)0,(byte)0,(byte)255);
+	public static final ColorRGBb YELLOW = new ColorRGBb((byte)255,(byte)255,(byte)0);
+	public static final ColorRGBb CYAN = new ColorRGBb((byte)0,(byte)255,(byte)255);
+	public static final ColorRGBb MAGENTA = new ColorRGBb((byte)255,(byte)0,(byte)255);
+	public static final ColorRGBb SILVER = new ColorRGBb((byte)192,(byte)192,(byte)192);
+	public static final ColorRGBb GRAY = new ColorRGBb((byte)128,(byte)128,(byte)128);
+	public static final ColorRGBb MAROON = new ColorRGBb((byte)128,(byte)0,(byte)0);
+	public static final ColorRGBb OLIVE = new ColorRGBb((byte)128,(byte)128,(byte)0);
+	public static final ColorRGBb GREEN = new ColorRGBb((byte)0,(byte)128,(byte)0);
+	public static final ColorRGBb PURPLE = new ColorRGBb((byte)128,(byte)0,(byte)128);
+	public static final ColorRGBb TEAL = new ColorRGBb((byte)0,(byte)128,(byte)128);
+	public static final ColorRGBb NAVY = new ColorRGBb((byte)0,(byte)0,(byte)128);
 	
 }
