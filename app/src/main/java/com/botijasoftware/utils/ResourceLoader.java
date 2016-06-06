b7d496a1f0e5424b3c0a132fca7a3ef42ba92135
botija <benedicto.miguel@gmail.com>
2016-06-07 00:02:18 +0200

    Codigo para carga de texturas y modelos

--- app/src/main/java/com/botijasoftware/utils/ResourceLoader.java
+++ app/src/main/java/com/botijasoftware/utils/ResourceLoader.java
@@ -18,6 +18,7 @@ public class ResourceLoader extends  Thread {
     private EGLConfig eglConfig;
     private EGLDisplay display;
     private Context androidContext;
+    private ResourceManager resourcemanager;
 
     private final Semaphore semaphore = new Semaphore(1, true);
     private boolean isLoading = false;
@@ -25,13 +26,28 @@ public class ResourceLoader extends  Thread {
     public class TextureInfo {
         int textureName;
         TextureOptions options;
+
+        public TextureInfo(int textureName, TextureOptions options) {
+            this.textureName = textureName;
+            this.options = options;
+        }
+    }
+
+    public class ModelInfo {
+        int  modelName;
+
+        public ModelInfo(int modelName) {
+            this.modelName = modelName;
+        }
     }
 
     private ArrayList<TextureInfo> texturesToLoad = new ArrayList<>();
+    private ArrayList<ModelInfo> modelsToLoad = new ArrayList<>();
 
 
-    public ResourceLoader(EGL14 egl, EGLContext renderContext, EGLDisplay display,
+    public ResourceLoader(ResourceManager resources, EGL14 egl, EGLContext renderContext, EGLDisplay display,
                          EGLConfig eglConfig, Context androidContext) {
+        this.resourcemanager = resources;
         this.egl = egl;
         this.display = display;
         this.eglConfig = eglConfig;
@@ -41,7 +57,7 @@ public class ResourceLoader extends  Thread {
     }
 
     public void run() {
-        int pbufferAttribs[] = { EGL14.EGL_WIDTH, 1, EGL14.EGL_HEIGHT, 1, EGL14.EGL_TEXTURE_TARGET,
+        int pbufferAttribs[] = { EGL14.EGL_CONTEXT_CLIENT_VERSION, 2, EGL14.EGL_WIDTH, 1, EGL14.EGL_HEIGHT, 1, EGL14.EGL_TEXTURE_TARGET,
                 EGL14.EGL_NO_TEXTURE, EGL14.EGL_TEXTURE_FORMAT, EGL14.EGL_NO_TEXTURE,
                 EGL14.EGL_NONE };
 
@@ -49,22 +65,80 @@ public class ResourceLoader extends  Thread {
 
         EGL14.eglMakeCurrent(display, localSurface, localSurface, textureContext);
 
-        //sleep(); // delay loading texture to demonstrate threaded loading
-
-    }
-
-    public void loadTexture() {
-
-
         try {
             semaphore.acquire();
+
+            while (texturesToLoad.size() > 0) {
+                TextureInfo ti = texturesToLoad.remove(0);
+                resourcemanager.loadTexture(ti.textureName, ti.options);
+            }
+
+            while (modelsToLoad.size() > 0) {
+                ModelInfo mi = modelsToLoad.remove(0);
+                resourcemanager.loadModel(mi.modelName);
+            }
+
+            semaphore.release();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+        //sleep(); // delay loading texture to demonstrate threaded loading
 
+    }
+
+    public void loadTexture(int textureName, TextureOptions options) {
+
+        try {
+            semaphore.acquire();
+            texturesToLoad.add(new TextureInfo(textureName, options));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         semaphore.release();
+    }
+
+    public void loadModel(int modelName) {
+
+        try {
+            semaphore.acquire();
+            modelsToLoad.add(new ModelInfo(modelName));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        semaphore.release();
+    }
+
+    public boolean finishedLoadTextures() {
+
+        boolean result = false;
+        try {
+            semaphore.acquire();
+            result = texturesToLoad.size() > 0;
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        semaphore.release();
+        return result;
+    }
+
+    public boolean finishedLoadModels() {
+
+        boolean result = false;
+        try {
+            semaphore.acquire();
+            result = modelsToLoad.size() > 0;
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        semaphore.release();
+        return result;
     }
 
     private void sleep() {
