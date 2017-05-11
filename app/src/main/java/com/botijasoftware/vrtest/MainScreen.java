bd50cd352b4372abca89ba5219106cc6c5db5b7e
botija <benedicto.miguel@gmail.com>
2017-05-12 00:35:15 +0200

    Matrix manual... funcionando

--- app/src/main/java/com/botijasoftware/vrtest/MainScreen.java
+++ app/src/main/java/com/botijasoftware/vrtest/MainScreen.java
@@ -1,10 +1,12 @@
 package com.botijasoftware.vrtest;
 
 import android.opengl.GLES20;
+import android.opengl.Matrix;
 
 import com.botijasoftware.utils.Camera;
 import com.botijasoftware.utils.ColorHSV;
 import com.botijasoftware.utils.ColorRGBA;
+import com.botijasoftware.utils.GLMatrix;
 import com.botijasoftware.utils.Mesh;
 import com.botijasoftware.utils.Model;
 import com.botijasoftware.utils.ResourceManager;
@@ -33,10 +35,14 @@ public class MainScreen extends Screen {
     int uvertex;
     int ucolor;
     int utextcoord;
-    int mMVMatrixUniformLocation;
-    int mPMatrixUniformLocation;
+    int unormal;
+    int mMVPMatrixUniformLocation;
     int texture0;
     Camera camera;
+    GLMatrix modelview_matrix = new GLMatrix();
+    GLMatrix projection_matrix =  new GLMatrix();
+    GLMatrix modelview_projection_matrix =  new GLMatrix();
+    GLMatrix model_matrix =  new GLMatrix();
 
     float angle = 0.0f;
     boolean resourcesloaded = false;
@@ -70,7 +76,7 @@ public class MainScreen extends Screen {
     public void Draw() {
 
     if (!resourcesloaded) {
-        model = mResourceManager.loadModel(R.raw.monkey);
+        model = mResourceManager.loadModel(R.raw.teapot);
         shader = new ShaderProgram(R.raw.shader_vs, R.raw.shader_ps);
         shader.LoadContent(mResourceManager);
 
@@ -78,8 +84,9 @@ public class MainScreen extends Screen {
         uvertex = GLES20.glGetAttribLocation(shaderid, "aVertexPosition");
         ucolor = GLES20.glGetAttribLocation(shaderid, "aVertexColor");
         utextcoord = GLES20.glGetAttribLocation(shaderid, "aVertexTextureCoord");
-        mMVMatrixUniformLocation = GLES20.glGetUniformLocation(shaderid, "uMVMatrix");
-        mPMatrixUniformLocation = GLES20.glGetUniformLocation(shaderid, "uPMatrix");
+        unormal = GLES20.glGetAttribLocation(shaderid, "aVertexNormal");
+        mMVPMatrixUniformLocation = GLES20.glGetUniformLocation(shaderid, "uMVPMatrix");
+
 
         texture0 = GLES20.glGetUniformLocation(shaderid, "texture0");
 
@@ -91,22 +98,24 @@ public class MainScreen extends Screen {
 
         shader.Use();
 
-        GLES20.glUniformMatrix4fv(mMVMatrixUniformLocation, 1, false, Renderer.modelview.matrix, 0);
-        GLES20.glUniformMatrix4fv(mPMatrixUniformLocation, 1, false, Renderer.projection.matrix, 0);
+        GLES20.glUniformMatrix4fv(mMVPMatrixUniformLocation, 1, false, modelview_projection_matrix.matrix, 0);
 
-        Viewport vw = new Viewport(0, 0, 500, 500);
+        Viewport vw = new Viewport(0, 0, 1920, 1080);
         //camera = new Camera(vw, new Vector3(0,0, 3), new Vector3(0, 1, 0), Vector3.UP);
         camera = new Camera(vw, new Vector3(0, 0, 10), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
         vw.enable();
-        camera.setPerspective(500,500);
+        camera.setPerspective(1920, 1080);
         Renderer.modelview.loadIdentity();
         // camera.setOrtho( width, height);
         //mCamera.setOrtho(gl, width, height);
-        GLES20.glDisable(GLES20.GL_DEPTH_TEST);
-        GLES20.glDisable(GLES20.GL_CULL_FACE);
+        //GLES20.glDisable(GLES20.GL_DEPTH_TEST);
+        //GLES20.glDisable(GLES20.GL_CULL_FACE);
         GLES20.glEnable(GLES20.GL_BLEND);
         GLES20.glBlendFunc(GLES20.GL_ALPHA, GLES20.GL_ONE_MINUS_DST_ALPHA);
-        GLES20.glDepthMask(false);
+        //GLES20.glDepthMask(false);
+        float ratio = (float) 1920 / 1080;
+        //Matrix.setIdentityM(projection_matrix.matrix, 0);
+        Matrix.frustumM(projection_matrix.matrix,0, -ratio, ratio, -1, 1, 1, 1000);
         //shader.enable();
         resourcesloaded = true;
     }
@@ -146,20 +155,27 @@ public class MainScreen extends Screen {
         if (angle > Math.PI * 2.0f)
             angle -= Math.PI * 2.0f;
 
-        camera.setLookAt(new Vector3(x, 0, z));
+        //camera.setLookAt(new Vector3(x, 0, z));
 
         //camera.set();
+        //modelview_matrix.setLookAt(0, 0, 10, x, 0, z, 0, 1, 0);
+        //Matrix.setIdentityM(modelview_matrix.matrix, 0);
+        Matrix.setLookAtM(modelview_matrix.matrix, 0, 0.0f, 0.0f, -10, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
 
-
-
+        Matrix.setIdentityM(model_matrix.matrix, 0);
+        Matrix.setRotateEulerM( model_matrix.matrix, 0, x, 1.0f, z);
+        GLMatrix tmp = new GLMatrix();
+        Matrix.multiplyMM(tmp.matrix, 0, modelview_matrix.matrix, 0, model_matrix.matrix,0);
+        Matrix.multiplyMM(modelview_projection_matrix.matrix, 0, projection_matrix.matrix, 0, tmp.matrix,0);
+        GLES20.glUniformMatrix4fv(mMVPMatrixUniformLocation, 1, false, modelview_projection_matrix.matrix, 0);
         /*Renderer.modelview.loadIdentity();
         angle += 1.0f;
         Renderer.modelview.rotate(angle, 0.5f, 0.5f, 0.5f);
         Renderer.modelview.scale(10, 10, 10);*/
 
         //Renderer.modelview.scale(100, 100, 100);
-        Renderer.modelview.loadIdentity();
-        camera.set();
+        //Renderer.modelview.loadIdentity();
+        //camera.set();
         for (int i = 0; i< model.mMesh.size(); i++) {
 
             Mesh m = model.mMesh.get(i);
--- app/src/main/res/raw/shader_vs.glsl
+++ app/src/main/res/raw/shader_vs.glsl
@@ -1,14 +1,14 @@
 attribute vec3 aVertexPosition;
 attribute vec4 aVertexColor;
+attribute vec3 aVertexNormal;
 attribute vec2 aVertexTextureCoord;
-uniform mat4 uMVMatrix;
-uniform mat4 uPMatrix;
+uniform mat4 uMVPMatrix;
 varying vec2 st;
 varying vec4 color;
 
 void main() {  
 	st = aVertexTextureCoord;
 	color = aVertexColor;
-	gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+	gl_Position = uMVPMatrix * vec4(aVertexPosition, 1.0);
 	//gl_Position = ftransform();
 }
